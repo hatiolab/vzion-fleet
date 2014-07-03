cf204740d55e21e577c33f3bf68fd0c350c3147c
heartyoh <heartyoh@gmail.com>
2014-07-04 00:27:48 +0800

    fix docker due to upgrade rails to 4.1.2

--- config/container/start-server.sh
+++ config/container/start-server.sh
@@ -9,6 +9,9 @@ source /etc/profile.d/rvm.sh
 # Reset database : generate database, table, seed data
 bundle exec rake hatio:reset RAILS_ENV=production
 
+# Due to upgrade to rails 4.1.2
+export SECRET_KEY_BASE=b4d7c3bc6ed805c1f4e9d167659f9b194ff46e7751f90b58b6868b1c3069df78097ede08680a4b4a17be6672dbd468645ab815442c5ce32969401616696241d3
+
 # Start Unicorn Server as a deamon
 bundle exec unicorn -D -p 8080 -E production
 
