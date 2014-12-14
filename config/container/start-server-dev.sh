d9a8287c7c2f231148b7b3b4247816c9c0f3f2db
Hearty, Oh <heartyoh@hatiolab.com>
2014-12-14 20:57:48 +0800

    Update start-server-dev.sh

--- config/container/start-server-dev.sh
+++ config/container/start-server-dev.sh
@@ -13,5 +13,4 @@ bundle exec rake hatio:reset RAILS_ENV=development
 export SECRET_KEY_BASE=b4d7c3bc6ed805c1f4e9d167659f9b194ff46e7751f90b58b6868b1c3069df78097ede08680a4b4a17be6672dbd468645ab815442c5ce32969401616696241d3
 
 # Start Unicorn Server as a deamon
-# bundle exec unicorn -D -p 3000 -E production
-bundle exec rails s
+bundle exec unicorn -D -p 3000 -E development
